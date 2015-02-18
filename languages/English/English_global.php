40b9daf8e865b49352f8888347d129adacd630ab
Lóna Lore <lonalore@freemail.hu>
2015-02-18 13:25:27 +0100

    initial commit

---
+++ languages/English/English_global.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Definition of english strings for common usage.
+ */
+
+define('LAN_PLUGIN__NCM_NAME', 'Node.js Chatbox Menu');
+define('LAN_PLUGIN__NCM_SUMM', 'Node.js integration');
+define('LAN_PLUGIN__NCM_DESC', 'This plugin implements chatbox_menu with Node.js.');
+define('LAN_PLUGIN__NCM_LINK', '');
